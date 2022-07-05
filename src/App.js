86ca809a2c6f192bc265053f671847b0e8d0d9c1
Cospis <qozimidris@gmail.com>
2022-07-05 08:03:38 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navigation from "./components/Navbar";
+
+function App() {
+  return <Navigation />;
+}
+
+export default App;
